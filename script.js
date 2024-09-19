ef4f96db58978d516f93780567e675d3255f1be7
unknown <morales.munoz.mily@gmail.com>
2024-09-18 21:02:52 -0600

    18/09/2024 David y Mily

---
+++ script.js
@@ -0,0 +1,32 @@
+//comentario en una sola linea
+
+/*
+comentario multi
+lineane
+as
+*/
+let nombre = "pensativa";
+const PI = 3.1416;
+var edad = 17;
+
+/*Tipos de datos:
+Cadena de texto (string): "HOLA", 'munod'
+Numero (number): 5,3,14
+Booleanos (booean): true , false
+*/
+
+//ejemplo 
+let mensaje = 'HOLA MUNDO';
+let numero = 42;
+let estudiante = true;
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a+b; //7
+let resta = a-b; //3
+let multiplicacion = a*b; //10
+let division = a/b; //2.5
+
+//git .add . / git commit -m "18/09/2024 David y Mily" 
+//git push 
