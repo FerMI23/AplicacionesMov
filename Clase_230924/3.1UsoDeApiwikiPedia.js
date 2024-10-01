fd00f58a7485bbc71fcf3092de1c9c39bc5facde
unknown <morales.munoz.mily@gmail.com>
2024-09-30 20:35:11 -0600

    gordita

--- Clase_230924/3.1UsoDeApiwikiPedia.js
+++ Clase_230924/3.1UsoDeApiwikiPedia.js
@@ -3,23 +3,18 @@ const url = `https://es.wikipedia.org/w/api.php?action=query&list=search&srsearc
 //Función para obtener resultados de Wikipedia
 async function buscarEnWikipedia () { 
 try { 
-const respuesta = await fetch (url); // Llamada a la API de 
-Wikipedia 
-JSON 
-const datos = await respuesta.json(); // Convierte la respuesta a 
+const respuesta = await fetch (url); // Llamada a la API de Wikipedia
+const datos = await respuesta.json(); // Convierte la respuesta a JSON 
 // Si hay resultados, mostrarlos en una lista 
 const resultados = datos.query.search;
 if (resultados.length > 0) { 
-document.getElementById('resultados').textContent = `Resultados 
-de búsqueda para "${terminoBusqueda}":`; 
+document.getElementById('resultados').textContent = `Resultados de búsqueda para "${terminoBusqueda}":`; 
 const listaResultados = 
 document.getElementById('listaResultados'); 
 // Recorrer los resultados y crear una lista de artículos 
 resultados.forEach (resultado => { 
 const li = document.createElement('li'); 
-li.innerHTML = `<a 
-href="https://es.wikipedia.org/wiki/${resultado.title}" 
-target="_blank">${resultado.title} </a> - ${resultado.snippet}`; 
+li.innerHTML = `<a href="https://es.wikipedia.org/wiki/${resultado.title}" target="_blank">${resultado.title} </a> - ${resultado.snippet}`; 
 listaResultados.appendChild(li); 
 }); 
 } else { 
@@ -29,7 +24,8 @@ document.getElementById('resultados').textContent = `No se encontraron resultado
 // En caso de error, mostrar un mensaje 
 'No se encontraron resultados'
 document.getElementById('resultados').textContent = `Error al obtener los datos de Wikipedia.`; 
-} 
-} 
 console.error ('Error:', error); 
+} 
+} 
 // Llamar a la función para hacer la búsqueda cuando la página se carga buscarEnWikipedia (); 
+buscarEnWikipedia();
