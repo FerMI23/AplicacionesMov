cd3785de0d6af1e1358a4a6fbab6109b2bc733f5
unknown <morales.munoz.mily@gmail.com>
2024-10-09 20:46:13 -0600

    comentado

--- Clase071024U2/script.js
+++ Clase071024U2/script.js
@@ -1,20 +1,20 @@
-function appendToDisplay (value) {
-    document.getElementById('display').value += value;
+function appendToDisplay (value) {// funcion para agregar un valor al display 
+    document.getElementById('display').value += value;//obtener el valor del display
 
 }
 
-function clearDisplay(){
-    document.getElementById('display'). value = '';
+function clearDisplay(){// agregar el valor de crear al display
+    document.getElementById('display'). value = '';//
 }
-function deleteLast(){
-    let displayValue = document.getElementById('display').value;
-    document.getElementById('display').value = displayValue.slice(0, -1);
+function deleteLast(){// funcion para eliminar el ultimo caracter del display
+    let displayValue = document.getElementById('display').value;// obtener el valor actual del dislay
+    document.getElementById('display').value = displayValue.slice(0, -1);// actualiza el valor del display
 }
-function calculateResult(){
+function calculateResult(){// funcion para calcular el resultado deñl display
     try{
-        let result = eval(document.getElementById('display').value);
-        document.getElementById('display').value = result;
+        let result = eval(document.getElementById('display').value);// actualiza el valor del display
+        document.getElementById('display').value = result;// da resultado de lo calculado
     } catch (error){;
-        alert('Expresión inválida');
+        alert('Expresión inválida');//
 }
 }
